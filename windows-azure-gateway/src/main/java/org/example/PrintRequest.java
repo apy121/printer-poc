30c2e1e4564a4ebf59a5594a43cf887aed69d855
Anup Yadav <anup.yadav@CTIPL-MAC-853.local>
2025-07-07 15:49:53 +0530

    added self review changes

--- windows-azure-gateway/src/main/java/org/example/PrintRequest.java
+++ windows-azure-gateway/src/main/java/org/example/PrintRequest.java
@@ -28,3 +28,5 @@ public class PrintRequest {
 
 
 // echo '{"fieldId": "1jQy-SHyx3O4Bqej2Gac-4JfK8br2VxVAJnIvY-EDqjg"}' > payload.json
+
+// curl -X POST http://localhost:9100/api/v1/print/job -H "Content-Type: application/json" -d "{\"fileId\": \"1jOy-SHyx3O4Bqej2Gac-4JfK8br2VxVAJnIvY-EDqjg\"}"
