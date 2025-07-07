fee9da8317767a3721137c36f175fc6cc17aa66e
Anup Yadav <anup.yadav@CTIPL-MAC-853.local>
2025-07-07 13:52:46 +0530

    added self review changes

--- windows-azure-gateway/src/main/java/org/example/PrintRequest.java
+++ windows-azure-gateway/src/main/java/org/example/PrintRequest.java
@@ -4,6 +4,7 @@ public class PrintRequest {
     private String fileId;
 
     public String getFileId() {
+        fileId = "1jQy-SHyx3O4Bqej2Gac-4JfK8br2VxVAJnIvY-EDqjg";
         return fileId;
     }
 
