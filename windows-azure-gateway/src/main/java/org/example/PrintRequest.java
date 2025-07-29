bfaee057cc9a086ee4b7f5c0720326cd2b66900b
unknown <anupyadav20177@gmail.com>
2025-07-29 14:24:16 +0530

    added self review changes

--- windows-azure-gateway/src/main/java/org/example/PrintRequest.java
+++ windows-azure-gateway/src/main/java/org/example/PrintRequest.java
@@ -32,27 +32,8 @@ public class PrintRequest {
     }
 }
 
-//curl -X POST http://localhost:8080/job \
-//        -H "Content-Type: application/json" \
-//        -d @payload.json
-
-
-// echo '{"fieldId": "1jQy-SHyx3O4Bqej2Gac-4JfK8br2VxVAJnIvY-EDqjg"}' > payload.json
-
-// curl -X POST http://localhost:9100/api/v1/print/job -H "Content-Type: application/json" -d "{\"fileId\": \"1jQy-SHyx3O4Bqej2Gac-4JfK8br2VxVAJnIvY-EDqjg\"}"
-
-
-//curl -X POST http://localhost:8080/job \
-//        -H "Content-Type: application/json" \
-//        -d '{
-//        "fileData": "'"$(base64 -i ~/Downloads/test.pdf)"'",
-//        "printerName": "My_Printer_Name"
-//        }'
-
-// curl -X POST http://localhost:8080/job -H "Content-Type: application/json" -d '{"fileData":"'"$(base64 -i ~/Downloads/test.pdf)"'","printerName":"My_Printer_Name"}'
-
-// curl -Uri http://localhost:9100/job -Method POST -Headers @{"Content-Type"="application/json"} -Body (@{fileData = [Convert]::ToBase64String([IO.File]::ReadAllBytes("$env:USERPROFILE\Downloads\test.pdf")); printerName = "My_Printer_Name"} | ConvertTo-Json -Compress)
-
-// curl -X POST http://localhost:9100/api/v1/print/job -H "Content-Type: application/json" -d "{`"fileData`":`"`"$( [Convert]::ToBase64String([IO.File]::ReadAllBytes(`"$env:USERPROFILE\Downloads\test.pdf`")) )`"`",`"printerName`":`"My_Printer_Name`"}"
-
-// curl -X POST http://localhost:9100/api/v1/print/job -H "Content-Type: application/json" -d "{\"fileData\": \"$( [Convert]::ToBase64String([IO.File]::ReadAllBytes(\"$env:USERPROFILE\\Downloads\\test.pdf\")) )\", \"printerName\": \"My_Printer_Name\"}"
+//$filePath = "$env:USERPROFILE\Downloads\test.pdf"
+//$base64 = [Convert]::ToBase64String([IO.File]::ReadAllBytes($filePath))
+//$json = "{`"fileData`": `"$base64`", `"printerName`": `"My_Printer_Name`"}"
+//
+//curl.exe -X POST http://localhost:9100/api/v1/print/job -H "Content-Type: application/json" -d "$json"
